54bc3af39dc7c6f8c3bc24d967960d0c55002120
SrPedro29 <pedroaugustocardoso.pac@gmail.com>
2026-02-28 19:04:13 -0300

    modificando a frase

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 function Home() {
     return <>
         <h1>Bem Vindo ao meu Projeto</h1>
-        <p>Xulinha eu te amo, coisa linda</p>
+        <p>Xulinha eu te amo, coisa linda. Se você me ama, dá uma risadinha</p>
         <p>Esse projeto tem o objetivo de mostrar como a programação pode criar novas possibilidades e soluções para problemas do dia a dia. Aplicações práticas e úteis desenvolvida de vários jeitos simples e criativas.</p>
     </>
 }
