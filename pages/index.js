511cb21a68e3cc7cfa0d00fb1e15f84277ec809b
SrPedro29 <pedroaugustocardoso.pac@gmail.com>
2026-02-28 19:16:25 -0300

    Modificando a frase e ajeitando pontuação

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 function Home() {
     return <>
         <h1>Bem Vindo ao meu Projeto</h1>
-        <p>Xulinha eu te amo, coisa linda. Se você me ama, dá uma risadinha</p>
+        <p>Xulinha, EU TE AMO, COISA LINDA. Se você me ama, dá uma risadinha! HIHIHI❤️</p>
         <p>Esse projeto tem o objetivo de mostrar como a programação pode criar novas possibilidades e soluções para problemas do dia a dia. Aplicações práticas e úteis desenvolvida de vários jeitos simples e criativas.</p>
     </>
 }
